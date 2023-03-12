82d5e540bcc9e9079a543b148de04c58f0693ce7
zzw <1830560034@qq.com>
2023-03-12 19:34:09 +0800

    JavaWeb 第一次提交

---
+++ 02_JavaScript/myself/fun1.js
@@ -0,0 +1,15 @@
+//无参函数
+function f1() {
+    alert("This is a parameterless function");
+}
+
+//有参函数
+function f2(a, b) {
+    alert("This is a parametric function. a=>" + a + ",b=>" + b);
+}
+
+//返回值函数
+function f3(a, b) {
+    alert("This is a function with return value.");
+    return a+b;
+}
